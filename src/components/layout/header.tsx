25fb561c564c1bb20c52f3444442fef206d94234
famous0811 <umh0811@naver.com>
2021-03-02 18:02:03 +0900

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./header.scss";
 
 function header() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <header>
+      <h2 id="logo">
+        <Link to="/">Bingo</Link>
+      </h2>
+      <ul className="menu">
+        <li>
+          <Link to="/rule">빙고 규칙</Link>
+        </li>
+        <li>
+          <Link to="/history">빙고 역사</Link>
+        </li>
+        <li>
+        <Link to="/gallery">빙고 갤러리</Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default header;
